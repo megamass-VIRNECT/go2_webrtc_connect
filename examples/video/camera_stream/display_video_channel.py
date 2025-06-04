bce7851d92e1474dbf3789d6b69fcbeb35d5226e
megamass <megamass@virnect.com>
2025-06-04 13:55:03 +0900

    수정

--- examples/video/camera_stream/display_video_channel.py
+++ examples/video/camera_stream/display_video_channel.py
@@ -15,6 +15,8 @@ from queue import Queue
 from go2_webrtc_driver.webrtc_driver import Go2WebRTCConnection, WebRTCConnectionMethod
 from aiortc import MediaStreamTrack
 
+import sys
+import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../../")))
 from load_webrtc_config import load_webrtc_config
 
