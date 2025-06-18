8e11506bc0fba5f71efdddded491ca9b30146782
megamass <megamass@virnect.com>
2025-06-18 15:20:01 +0900

    수정 0618-4

--- go2_webrtc_driver/unitree_auth.py
+++ go2_webrtc_driver/unitree_auth.py
@@ -91,6 +91,7 @@ def make_remote_request(path, body, token, method="GET"):
         response = requests.post(url, data=encoded_body, headers=headers)
 
     # Return the response as JSON
+    print(f"make_remote_request() response: {response}")
     return response.json()
 
 def make_local_request(path, body=None, headers=None):
