44871e961c2daeb356a6138ce2264e9236d87510
megamass <megamass@virnect.com>
2025-06-27 14:37:33 +0900

    0627-8

--- api/webrtc_api.py
+++ api/webrtc_api.py
@@ -78,7 +78,7 @@ def send_local_offer():
     try:
         # send SDP offer to local peer
         sdp_offer_json = {
-            "id": "STA_localNetwork",
+            "id": "",
             # "turnserver": local_turn_server_info,
             "sdp": local_description["sdp"],
             "type": local_description["type"],
